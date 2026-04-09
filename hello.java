cc592f65b80b7a8419c8e8fb9e1f221d73f6c06d
Bug-slayers-team <prerana.wagh@cumminscollege.in>
2026-04-09 23:02:38 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ public class hello{
     public static void main(String s[])
     {
         System.out.println("Hello Piyush");
+        System.out.println("Hello prerna");
+        System.out.println("Hello preyush");
     }
 }
